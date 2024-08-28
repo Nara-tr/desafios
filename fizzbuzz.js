25f95c40320f38681571e1ec67aeaa272c7344a6
Thaynara <thaynara.ramos.tr@gmail.com>
2024-08-27 23:57:18 -0300

    refatora fizzbuzz para uso de function auxiliar

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,24 @@
-let numero = 5
+function calculateRemainderDivision(number, divisibleNumber) {
+    const calculationResult = number % divisibleNumber == 0
+    return calculationResult
+}
+
+
+let numero = 4
 const numeroDivisivelTres = 3
 const numeroDivisivelCinco = 5
 // verifica o resto da divisão entre numero e numero divisivel
-let resultadoDivisaoPorTres = numero % numeroDivisivelTres == 0
-let resultadoDivisaPorCinco = numero % numeroDivisivelCinco == 0
-let mensagemErro = " Esse número não é divisivel nem por 3 nem por 5"
+let resultadoDivisaoPorTres = calculateRemainderDivision(numero, numeroDivisivelTres)
+let resultadoDivisaPorCinco = calculateRemainderDivision(numero, numeroDivisivelCinco)
+let mensagemErro = " Esse número não é divisível nem por 3 nem por 5"
 
 if(resultadoDivisaoPorTres) {
     const mensagemEhDivisivelPorTres = "Fizz"   
     console.log(mensagemEhDivisivelPorTres)
-} else if (resultadoDivisaPorCinco){
+} else if (resultadoDivisaPorCinco) {
     const mensagemEhDivisivelPorCinco = "Buzz"
     console.log(mensagemEhDivisivelPorCinco)
 } else {
     console.log(mensagemErro)
 }
+
