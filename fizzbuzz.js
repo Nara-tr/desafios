6595d95ca8ce6790fc74709c5f80aeabb83e38bd
Thaynara <thaynara.ramos.tr@gmail.com>
2024-08-27 23:03:02 -0300

    incluindo divisao por 5

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,17 @@
-let numero = 3
+let numero = 5
 const numeroDivisivelTres = 3
 const numeroDivisivelCinco = 5
 // verifica o resto da divisão entre numero e numero divisivel
-let resultadoDivisao = numero % numeroDivisivelTres == 0
+let resultadoDivisaoPorTres = numero % numeroDivisivelTres == 0
+let resultadoDivisaPorCinco = numero % numeroDivisivelCinco == 0
 let mensagemErro = " Esse número não é divisivel nem por 3 nem por 5"
 
-if(resultadoDivisao) {
+if(resultadoDivisaoPorTres) {
     const mensagemEhDivisivelPorTres = "Fizz"   
     console.log(mensagemEhDivisivelPorTres)
+} else if (resultadoDivisaPorCinco){
+    const mensagemEhDivisivelPorCinco = "Buzz"
+    console.log(mensagemEhDivisivelPorCinco)
 } else {
     console.log(mensagemErro)
 }
