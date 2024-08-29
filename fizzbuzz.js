8cefc5bde93b2f3a89786f9ff3d6e41d00b017de
Thaynara <thaynara.ramos.tr@gmail.com>
2024-08-28 22:38:05 -0300

    Desafio finalizado - simples

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,22 +3,26 @@ function calculateRemainderDivision(number, divisibleNumber) {
     return calculationResult
 }
 
-
-let numero = 4
-const numeroDivisivelTres = 3
-const numeroDivisivelCinco = 5
-// verifica o resto da divisão entre numero e numero divisivel
-let resultadoDivisaoPorTres = calculateRemainderDivision(numero, numeroDivisivelTres)
-let resultadoDivisaPorCinco = calculateRemainderDivision(numero, numeroDivisivelCinco)
-let mensagemErro = " Esse número não é divisível nem por 3 nem por 5"
-
-if(resultadoDivisaoPorTres) {
-    const mensagemEhDivisivelPorTres = "Fizz"   
-    console.log(mensagemEhDivisivelPorTres)
-} else if (resultadoDivisaPorCinco) {
-    const mensagemEhDivisivelPorCinco = "Buzz"
-    console.log(mensagemEhDivisivelPorCinco)
-} else {
-    console.log(mensagemErro)
+function fizzBuzz(number) {
+    const numeroDivisivelTres = 3
+    const numeroDivisivelCinco = 5
+    // verifica o resto da divisão entre numero e numero divisivel
+    let resultadoDivisaoPorTres = calculateRemainderDivision(number, numeroDivisivelTres)
+    let resultadoDivisaoPorCinco = calculateRemainderDivision(number, numeroDivisivelCinco)
+    let mensagemErro = " Esse número não é divisível nem por 3 nem por 5"
+    
+    if(resultadoDivisaoPorTres && resultadoDivisaoPorCinco) {
+        const mensagemEhDivisivelPorTresEhCinco = "Fizzbuzz"
+        console.log(mensagemEhDivisivelPorTresEhCinco)
+    } else if (resultadoDivisaoPorTres) {
+        const mensagemEhDivisivelPorTres = "Fizz"   
+        console.log(mensagemEhDivisivelPorTres)
+    }  else if (resultadoDivisaoPorCinco) {
+        const mensagemEhDivisivelPorCinco = "Buzz"
+        console.log(mensagemEhDivisivelPorCinco)
+    }  else {
+        console.log(mensagemErro)
+    }
 }
 
+fizzBuzz(15)
